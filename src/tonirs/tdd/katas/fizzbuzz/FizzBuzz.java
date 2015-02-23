bf3258eb52385dc6304643114f585ccbfda3d3ce
tonirs <tonirs@gmail.com>
2015-02-23 23:26:59 +0100

    Makes FizzBuzz work for 1.

---
+++ src/tonirs/tdd/katas/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,9 @@
+package tonirs.tdd.katas.fizzbuzz;
+
+public class FizzBuzz {
+
+    public final String translate(final int number) {
+        return "1";
+    }
+
+}
---
+++ test/tonirs/tdd/katas/fizzbuzz/FizzBuzzTests.java
@@ -0,0 +1,24 @@
+package tonirs.tdd.katas.fizzbuzz;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class FizzBuzzTests {
+
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    public void numberNeitherMultipleOfThreeNorFiveTranslatesToItself() {
+        assertThat(fizzBuzz.translate(1), is(equalTo("1")));
+    }
+
+}
